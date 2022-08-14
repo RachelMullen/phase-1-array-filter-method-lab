fa565639d585a0926863e7a61125b047a02e0a58
Rachel Mullen <mullen.rachel@gmail.com>
2022-08-14 13:40:42 -0600

    commit tree 70847c8ac7dd55d6bf42c6896d36f49dbd5207fb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+
+function findMatching(names, string) {
+    return names.filter(
+        (letters) => letters.toLowerCase() === string.toLowerCase()
+    );
+}
+
+// function fuzzyMatch(names,provided) {
+//     return names.filter(
+//         (possible) => 
+// possible.toLowercase().indexOf(provided.toLowercase()) === 0
+//     );
+// }
+
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+
+function matchName(names, string) {
+ return names.filter((record) => record.name === string);
+}
